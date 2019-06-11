1a6f6257c5451d75df94a61fb333a77bbdc06f3b
jimmyblylee <jimmyblylee@126.com>
2019-06-12 03:56:15 +0800

    标题+基础信息

---
+++ theme/src/assets/js/theme/demos/demo-hn-01/main.js
@@ -0,0 +1,25 @@
+/* ***************************************************************************
+ * Copyright (C) 2018-2019 the original author Jimmybly Lee
+ * or authors of NAPTUNE.COM
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of MIT License as published by
+ * the Free Software Foundation;
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the MIT License for more details.
+ *
+ * You should have received a copy of the MIT License along
+ * with this library; if not, write to the Free Software Foundation.
+ * ***************************************************************************/
+
+/**
+ * Description: Main.<br>
+ * Created by jimmy on 2019/6/11.
+ * @author Jimmybly Lee
+ */
+angular.module("WebAppICBS", [
+
+]);
